8ccc7ceec5bfceaff2285c522bbff666e8c5daf4
kon23001@byui.edu <kon23001@byui.edu>
2024-06-16 11:33:02 +0700

    Complete Learning Activity 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Humanitarian Translator";
+        job1._company = "HR Inc";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Humanitarian Specialist";
+        job2._company = "Association";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Channak Kong";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
