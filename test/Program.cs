3430589bd658123a74bb44f0d75d16f09ddf70f1
kon23001@byui.edu <kon23001@byui.edu>
2024-06-28 00:47:34 +0700

    Complet Learning 03

---
+++ test/Program.cs
@@ -0,0 +1,32 @@
+﻿public abstract class PaymentMethod
+{
+    public abstract void MakePayment(decimal amount);
+    public abstract string GetPaymentDetails();
+}
+
+public class CreditCardPayment : PaymentMethod
+{
+    public override void MakePayment(decimal amount)
+    {
+        // Implement credit card payment logic
+    }
+
+    public override string GetPaymentDetails()
+    {
+        // Return credit card payment details
+    }
+}
+
+public class PayPalPayment : PaymentMethod
+{
+    public override void MakePayment(decimal amount)
+    {
+        // Implement PayPal payment logic
+    }
+
+    public override string GetPaymentDetails()
+    {
+        // Return PayPal payment details
+    }
+}
+
