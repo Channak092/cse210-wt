5f04930622685a412a33542e455fb1839bf425b0
kon23001@byui.edu <kon23001@byui.edu>
2024-07-11 01:05:47 +0700

    Learning5 is completed.

--- test/Program.cs
+++ test/Program.cs
@@ -1,42 +1,10 @@
-﻿public class Employee  
-{  
-    // Private field to store employee's salary  
-    private decimal _salary;  
-  
-    // Public property to get and set employee's salary  
-    public decimal Salary  
-    {  
-        get { return _salary; }  
-        set  
-        {  
-            if (value &lt; 0)  
-            {  
-                throw new ArgumentException("Salary cannot be negative");  
-            }  
-            _salary = value;  
-        }  
-    }  
-  
-    // Public method to increase salary by a certain percentage  
-    public void IncreaseSalary(decimal percentage)  
-    {  
-        _salary += (_salary * (percentage / 100));  
-    }  
-  
-    // Public method to display employee's salary  
-    public void DisplaySalary()  
-    {  
-        Console.WriteLine("Employee's salary is: " + _salary);  
-    }  
-}  
-  
-class Program  
-{  
-    static void Main(string[] args)  
-    {  
-        Employee emp = new Employee();  
-        emp.Salary = 50000; // Set salary through public property  
-        emp.IncreaseSalary(10); // Increase salary by 10%  
-        emp.DisplaySalary(); // Display updated salary  
-    }  
-}  
+﻿// a parent class
+public class Employee
+{
+  private float salary = 100f;
+
+  public float CalculatePay()
+  {
+    return salary;
+  }
+}
